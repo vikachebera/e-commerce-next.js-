d0a0e5ed20dbf01bfff1ef6374e06e21c426e52f
Viktoriia Chebera <cheberavika@gmail.com>
2025-05-17 16:30:48 +0200

    тестування роботи ci

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -2,8 +2,6 @@ import {screen} from "@testing-library/dom";
 import {render} from "@testing-library/react";
 import Home from "@/app/page";
 
-it('should have Examples text', () => {
-    render(<Home/>)
-    const myElem = screen.getByText('Examples')
-    expect(myElem).toBeInTheDocument();
-})
+test('Home page renders without errors', () => {
+    render(<Home />);
+});
