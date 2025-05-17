49e2f38ce82e19d0f143e8b58d4343cbfbd06ad1
Viktoriia Chebera <cheberavika@gmail.com>
2025-05-17 21:07:56 +0200

    prisma generate

--- generate/client/edge.js
+++ generate/client/edge.js
@@ -205,7 +205,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
