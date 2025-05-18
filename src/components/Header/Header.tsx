91b6d3adc9ffcef6230f0f6083fa67f9bb4ba261
Viktoriia Chebera <cheberavika@gmail.com>
2025-05-18 20:41:54 +0200

    del error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import {getServerSession} from "next-auth";
+import { getServerSession } from "next-auth/next";
 import {authOptions} from "@/app/api/auth/[...nextauth]/options";
 
 export default  async function Header() {
--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,4 +1,4 @@
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 export const getUserSession = async () => {
