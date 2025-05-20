ad6eaa4ea76255e5952d7a7658d6431e7726283b
Viktoriia Chebera <cheberavika@gmail.com>
2025-05-20 14:07:04 +0200

    додано сторінкку про замовлення користувача та виправлено помилку з авторизацією через провайдер

---
+++ prisma/seed.ts
@@ -0,0 +1,97 @@
+import prisma from "../src/lib/prisma.ts";
+
+async function main() {
+    const categories = await prisma.category.createMany({
+        data: [
+            { name: "Смартфони" },
+            { name: "Ноутбуки" },
+            { name: "Побутова техніка" },
+        ],
+        skipDuplicates: true,
+    });
+    console.log(`Створено ${categories.count} категорій`);
+
+    const products = await prisma.product.createMany({
+        data: [
+            {
+                name: "iPhone 15 Pro",
+                price: 1099.99,
+                description: "Флагманський смартфон Apple",
+                categoryId: 1,
+            },
+            {
+                name: "iPhone 15 ",
+                price: 990.99,
+                description: "Флагманський смартфон Apple",
+                categoryId: 1,
+            },
+            {
+                name: "iPhone 13",
+                price: 500.99,
+                description: "Флагманський смартфон Apple",
+                categoryId: 1,
+            },
+            {
+                name: "MacBook Air M2",
+                price: 1299.99,
+                description: "Легкий та потужний ноутбук",
+                categoryId: 2,
+            },
+        ],
+        skipDuplicates: true,
+    });
+    console.log(`Створено ${products.count} продуктів`);
+
+    await createOrder();
+}
+
+async function createOrder() {
+    const userId = 1; // заміни на реального користувача
+
+    const orderItemsData = [
+        {
+            productId: 1,
+            quantity: 2,
+            price: 1099.99,
+        },
+        {
+            productId: 3,
+            quantity: 1,
+            price: 500.99,
+        },
+    ];
+
+    const total = orderItemsData.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+    );
+
+    const newOrder = await prisma.order.create({
+        data: {
+            userId,
+            total,
+            status: "PENDING",
+            orderItems: {
+                create: orderItemsData.map((item) => ({
+                    productId: item.productId,
+                    quantity: item.quantity,
+                    price: item.price,
+                })),
+            },
+        },
+        include: {
+            orderItems: true,
+        },
+    });
+
+    console.log("✅ Замовлення створено:", newOrder);
+}
+
+main()
+    .catch((e) => {
+        console.error("❌ Помилка при заповненні БД:", e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
