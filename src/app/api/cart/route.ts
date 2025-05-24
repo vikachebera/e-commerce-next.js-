b5415d9ac6b9c953cc644ee549318206ecc4bd4f
Viktoriia Chebera <cheberavika@gmail.com>
2025-05-24 13:59:27 +0200

    deleted useless req

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,9 +1,9 @@
-import {NextRequest, NextResponse} from 'next/server';
+import { NextResponse} from 'next/server';
 import {getServerSession} from 'next-auth';
 import {authOptions} from '@/app/api/auth/[...nextauth]/options';
 import prisma from '@lib/prisma';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
     const session = await getServerSession(authOptions);
 
     if (!session?.user?.id) {
